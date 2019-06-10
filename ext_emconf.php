164e30b5b0b61cc4ef199b7eaf457c1033963dba
Chris Müller <chris@krue.ml>
2019-06-10 17:03:23 +0200

    [TASK] Adjust some version information

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,15 +4,15 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Embedding schema.org vocabulary',
     'description' => 'schema.org structured data for your website',
     'category' => 'fe',
-    'state' => 'stable',
+    'state' => 'beta',
     'createDirs' => '',
     'clearCacheOnLoad' => true,
     'author' => 'Chris Müller',
     'author_email' => 'typo3@krue.ml',
-    'version' => '1.0.0',
+    'version' => '0.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99',
+            'typo3' => '9.5.0-9.5.99',
         ],
         'conflicts' => [
         ],
