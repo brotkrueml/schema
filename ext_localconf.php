664d13f35ce6fbb9d3be90c3fa1849d4e1a5a562
Chris Müller <chris@krue.ml>
2019-06-10 17:06:12 +0200

    [TASK] Wrap in function

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,10 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-postProcess'][] =
-    \Brotkrueml\Schema\Hook\PageRenderer\PostProcessHook::class . '->execute';
+call_user_func(function () {
+    /** @noinspection PhpFullyQualifiedNameUsageInspection */
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-postProcess'][] =
+        \Brotkrueml\Schema\Hook\PageRenderer\PostProcessHook::class . '->execute';
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['schema'] = ['Brotkrueml\\Schema\\ViewHelper'];
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['schema'] = ['Brotkrueml\\Schema\\ViewHelper'];
+});
