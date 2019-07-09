068d2586e5a4b38a63407199c227d5e7a69e3baf
Chris Müller <cmueller@jobrouter.de>
2019-07-09 10:58:49 +0200

    [TASK] Possibility to overwrite web page type in another language

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -7,14 +7,16 @@ call_user_func(function () {
     $fields = [
         'tx_schema_webpagetype' => [
             'exclude' => true,
-            'l10n_mode' => 'exclude',
             'label' => 'LLL:EXT:schema/Resources/Private/Language/locallang_db.xlf:pages.tx_schema_webpagetype',
             'config' => [
                 'type' => 'select',
                 'renderType' => 'selectSingle',
                 'items' => \Brotkrueml\Schema\Provider\WebPageTypeProvider::getTypesForTcaSelect(),
                 'size' => 1,
-                'maxitems' => 1
+                'maxitems' => 1,
+                'behaviour' => [
+                    'allowLanguageSynchronization' => true,
+                ],
             ],
         ],
     ];
