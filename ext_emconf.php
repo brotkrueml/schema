c0523ba34f387ba0661e03d41e7992d79a2b6490
Chris Müller <chris@krue.ml>
2019-07-27 17:03:13 +0200

    [TASK] Compatibility with TYPO3 10.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.5.99',
+            'typo3' => '9.5.0-10.0.99',
         ],
         'conflicts' => [
             'sdbreadcrumb' => '',
