dfb99226418fb6e3d0f7fa6b3e1e7df6fb70ecdf
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-06-27 16:21:27 +0200

    [TASK] Provide actions-image icon for TYPO3 v9
    Related: #49

--- ext_tables.php
+++ ext_tables.php
@@ -2,6 +2,11 @@
 defined('TYPO3_MODE') || die('Access denied.');
 
 (function () {
+    if (!TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('adminpanel')) {
+        return;
+    }
+
+    /** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
     $iconRegistry = TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(TYPO3\CMS\Core\Imaging\IconRegistry::class);
     foreach (['documentation-google', 'documentation-schema', 'module-adminpanel'] as $icon) {
         $iconRegistry->registerIcon(
@@ -12,6 +17,20 @@ defined('TYPO3_MODE') || die('Access denied.');
                     'EXT:%s/Resources/Public/Icons/%s.svg',
                     Brotkrueml\Schema\Extension::KEY,
                     $icon
+                ),
+            ]
+        );
+    }
+
+    if (!$iconRegistry->isRegistered('actions-image')) {
+        // TYPO3 version 9 does not ship the image icon
+        $iconRegistry->registerIcon(
+            'actions-image',
+            TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+            [
+                'source' => sprintf(
+                    'EXT:%s/Resources/Public/Icons/actions-image.svg',
+                    Brotkrueml\Schema\Extension::KEY
                 ),
             ]
         );
