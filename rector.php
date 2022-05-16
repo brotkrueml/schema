a10d375eb69a6ac06d56437c9f224eb8c0b95dc2
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-05-16 18:14:47 +0200

    [TASK] Update rector to version 0.12.23

--- rector.php
+++ rector.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\DeadCode\Rector\Cast\RecastingRemovalRector;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPromotedPropertyRector;
@@ -16,32 +16,28 @@ use Rector\TypeDeclaration\Rector\ClassMethod\AddArrayParamDocTypeRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddArrayReturnDocTypeRector;
 use Rector\TypeDeclaration\Rector\FunctionLike\ReturnTypeDeclarationRector;
 use Rector\TypeDeclaration\Rector\FunctionLike\ParamTypeDeclarationRector;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_74);
-    $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(SetList::DEAD_CODE);
-    $containerConfigurator->import(SetList::EARLY_RETURN);
-    $containerConfigurator->import(SetList::TYPE_DECLARATION);
-    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
-    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_EXCEPTION);
-    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_MOCK);
-    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_SPECIFIC_METHOD);
-    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_YIELD_DATA_PROVIDER);
+return static function (RectorConfig $config): void {
+    $config->import(LevelSetList::UP_TO_PHP_74);
+    $config->import(SetList::CODE_QUALITY);
+    $config->import(SetList::DEAD_CODE);
+    $config->import(SetList::EARLY_RETURN);
+    $config->import(SetList::TYPE_DECLARATION);
+    $config->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
+    $config->import(PHPUnitSetList::PHPUNIT_EXCEPTION);
+    $config->import(PHPUnitSetList::PHPUNIT_SPECIFIC_METHOD);
+    $config->import(PHPUnitSetList::PHPUNIT_YIELD_DATA_PROVIDER);
 
-    $parameters = $containerConfigurator->parameters();
+    $config->phpVersion(PhpVersion::PHP_74);
 
-    $parameters->set(Option::PATHS, [
+    $config->autoloadPaths([
+        __DIR__ . '/.Build/vendor/autoload.php',
+    ]);
+    $config->paths([
         __DIR__ . '/Classes',
         __DIR__ . '/Tests',
     ]);
-
-    $parameters->set(Option::AUTOLOAD_PATHS, [__DIR__ . '/.Build/vendor/autoload.php']);
-
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_74);
-
-    $parameters->set(Option::SKIP, [
+    $config->skip([
         __DIR__ . '/Classes/Model/Type/*',
         __DIR__ . '/Classes/ViewHelpers/Type/*',
         AddArrayParamDocTypeRector::class => [
