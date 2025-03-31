c413654160f237c9cab123f3027bf5652a802025
Chris Müller <coding@brotkrueml.dev>
2025-03-31 20:54:05 +0200

    test: provide custom FunctionalTestBootstrap.php file
    This is advised by the testing framework. Additionally, we can get rid of the
    separate RegisterTypoe3Constants.php file as the registration of the constant
    is now done in our custom bootstrap file.

---
+++ Tests/FunctionalTestsBootstrap.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the "schema" extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+use TYPO3\TestingFramework\Core\Testbase;
+
+/*
+ * This file is part of the "schema" extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+/**
+ * Copied from typo3/testing-framework/Resources/Core/Build/FunctionalTestsBootstrap.php
+ * and adapted.
+ */
+(static function (): void {
+    $testbase = new Testbase();
+    $testbase->defineOriginalRootPath();
+    $testbase->createDirectory(ORIGINAL_ROOT . 'typo3temp/var/tests');
+    $testbase->createDirectory(ORIGINAL_ROOT . 'typo3temp/var/transient');
+
+    // Needed for PageRenderer mock
+    defined('LF') ?: define('LF', chr(10));
+})();
