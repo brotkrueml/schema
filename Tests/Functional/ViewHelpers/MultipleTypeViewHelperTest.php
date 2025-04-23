22fdcf7b15b054d36a3c3516363aea4517b7366c
Chris Müller <coding@brotkrueml.dev>
2025-04-23 19:52:20 +0200

    chore: utilise RenderingContextFactory and TemplateView in MultipleTypeViewHelperTest

--- Tests/Functional/ViewHelpers/MultipleTypeViewHelperTest.php
+++ Tests/Functional/ViewHelpers/MultipleTypeViewHelperTest.php
@@ -12,42 +12,35 @@ declare(strict_types=1);
 namespace Brotkrueml\Schema\Tests\Functional\ViewHelpers;
 
 use Brotkrueml\Schema\Extension;
-use Brotkrueml\Schema\Tests\Helper\SchemaCacheTrait;
-use Brotkrueml\Schema\Tests\Helper\TypeProviderWithFixturesTrait;
-use Brotkrueml\Schema\Type\TypeProvider;
+use Brotkrueml\Schema\Manager\SchemaManager;
 use Brotkrueml\Schema\ViewHelpers\MultipleTypeViewHelper;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextFactory;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\Exception;
+use TYPO3Fluid\Fluid\View\TemplateView;
 
 #[CoversClass(MultipleTypeViewHelper::class)]
-final class MultipleTypeViewHelperTest extends ViewHelperTestCase
+final class MultipleTypeViewHelperTest extends FunctionalTestCase
 {
-    use SchemaCacheTrait;
-    use TypeProviderWithFixturesTrait;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->defineCacheStubsWhichReturnEmptyEntry();
-
-        GeneralUtility::setSingletonInstance(TypeProvider::class, $this->getTypeProvider());
-    }
-
-    protected function tearDown(): void
-    {
-        GeneralUtility::purgeInstances();
-        parent::tearDown();
-    }
+    protected array $testExtensionsToLoad = [
+        'brotkrueml/schema',
+    ];
 
     #[Test]
     #[DataProvider('fluidTemplatesProvider')]
     public function itBuildsSchemaCorrectly(string $template, string $expected): void
     {
-        $this->renderTemplate($template, []);
-        $actual = $this->schemaManager->renderJsonLd();
+        /** @var RenderingContextInterface $context */
+        $context = $this->get(RenderingContextFactory::class)->create();
+        $context->getTemplatePaths()->setTemplateSource($template);
+
+        (new TemplateView($context))->render();
+
+        $actual = $this->get(SchemaManager::class)->renderJsonLd();
 
         self::assertSame(\sprintf(Extension::JSONLD_TEMPLATE, $expected), $actual);
     }
@@ -55,82 +48,82 @@ final class MultipleTypeViewHelperTest extends ViewHelperTestCase
     public static function fluidTemplatesProvider(): iterable
     {
         yield 'Using view helper without properties' => [
-            'template' => '<schema:multipleType types="ProductStub,ServiceStub"/>',
-            'expected' => '{"@context":"https://schema.org/","@type":["ProductStub","ServiceStub"]}',
+            'template' => '<schema:multipleType types="Product,Service"/>',
+            'expected' => '{"@context":"https://schema.org/","@type":["Product","Service"]}',
         ];
 
         yield 'Using view helper with properties argument' => [
-            'template' => <<<EOF
+            'template' => <<<TEMPLATE
                 <schema:multipleType
-                    types="ProductStub,ServiceStub"
+                    types="Product,Service"
                     properties="{
                         name: 'some name',
                         sku: 'some sku',
                         serviceType: 'some service type'
                     }"
                 />
-             EOF,
-            'expected' => '{"@context":"https://schema.org/","@type":["ProductStub","ServiceStub"],"name":"some name","serviceType":"some service type","sku":"some sku"}',
+             TEMPLATE,
+            'expected' => '{"@context":"https://schema.org/","@type":["Product","Service"],"name":"some name","serviceType":"some service type","sku":"some sku"}',
         ];
 
         yield 'Using view helper with PropertyViewHelper for properties' => [
-            'template' => <<<EOF
-                <schema:multipleType types="ProductStub,ServiceStub">
+            'template' => <<<TEMPLATE
+                <schema:multipleType types="Product,Service">
                     <schema:property -as="name" value="this name"/>
                     <schema:property -as="sku" value="this sku"/>
                     <schema:property -as="serviceType" value="this service type"/>
                 </schema:multipleType>',
-            EOF,
-            'expected' => '{"@context":"https://schema.org/","@type":["ProductStub","ServiceStub"],"name":"this name","serviceType":"this service type","sku":"this sku"}',
+            TEMPLATE,
+            'expected' => '{"@context":"https://schema.org/","@type":["Product","Service"],"name":"this name","serviceType":"this service type","sku":"this sku"}',
         ];
 
         yield 'Using view helper with properties argument and PropertyViewHelper for properties' => [
-            'template' => <<<EOF
-                <schema:multipleType types="ProductStub,ServiceStub" properties="{name: 'that name'}">
+            'template' => <<<TEMPLATE
+                <schema:multipleType types="Product,Service" properties="{name: 'that name'}">
                     <schema:property -as="sku" value="that sku"/>
                     <schema:property -as="serviceType" value="that service type"/>
                 </schema:multipleType>',
-            EOF,
-            'expected' => '{"@context":"https://schema.org/","@type":["ProductStub","ServiceStub"],"name":"that name","serviceType":"that service type","sku":"that sku"}',
+            TEMPLATE,
+            'expected' => '{"@context":"https://schema.org/","@type":["Product","Service"],"name":"that name","serviceType":"that service type","sku":"that sku"}',
         ];
 
         yield 'Using view helper with id' => [
-            'template' => '<schema:multipleType types="ProductStub,ServiceStub" -id="some-id"/>',
-            'expected' => '{"@context":"https://schema.org/","@type":["ProductStub","ServiceStub"],"@id":"some-id"}',
+            'template' => '<schema:multipleType types="Product,Service" -id="some-id"/>',
+            'expected' => '{"@context":"https://schema.org/","@type":["Product","Service"],"@id":"some-id"}',
         ];
 
         yield 'Using view helper as child of another type view helper' => [
-            'template' => <<<EOF
+            'template' => <<<TEMPLATE
                 <schema:type.thing>
                     <schema:multipleType
-                        types="ProductStub,ServiceStub"
+                        types="Product,Service"
                         properties="{name: 'child name'}"
                         -as="subjectOf"
                     />
                 </schema:type.thing>
-            EOF,
-            'expected' => '{"@context":"https://schema.org/","@type":"Thing","subjectOf":{"@type":["ProductStub","ServiceStub"],"name":"child name"}}',
+            TEMPLATE,
+            'expected' => '{"@context":"https://schema.org/","@type":"Thing","subjectOf":{"@type":["Product","Service"],"name":"child name"}}',
         ];
 
         yield 'Using view helper as main entity of page' => [
-            'template' => <<<EOF
+            'template' => <<<TEMPLATE
                 <schema:type.webPage/>
                 <schema:multipleType
-                    types="ProductStub,ServiceStub"
+                    types="Product,Service"
                     properties="{name: 'child name'}"
                     -isMainEntityOfWebPage="1"
                 />
-            EOF,
-            'expected' => '{"@context":"https://schema.org/","@type":"WebPage","mainEntity":{"@type":["ProductStub","ServiceStub"],"name":"child name"}}',
+            TEMPLATE,
+            'expected' => '{"@context":"https://schema.org/","@type":"WebPage","mainEntity":{"@type":["Product","Service"],"name":"child name"}}',
         ];
 
         yield 'View helper inside "for" loop' => [
-            'template' => <<<EOF
+            'template' => <<<TEMPLATE
                 <f:for each="{0: 'foo', 1: 'bar', 2: 'qux'}" as="item">
-                    <schema:multipleType types="ProductStub,ServiceStub" properties="{name: '{item}'}"/>
+                    <schema:multipleType types="Product,Service" properties="{name: '{item}'}"/>
                 </f:for>',
-            EOF,
-            'expected' => '{"@context":"https://schema.org/","@graph":[{"@type":["ProductStub","ServiceStub"],"name":"foo"},{"@type":["ProductStub","ServiceStub"],"name":"bar"},{"@type":["ProductStub","ServiceStub"],"name":"qux"}]}',
+            TEMPLATE,
+            'expected' => '{"@context":"https://schema.org/","@graph":[{"@type":["Product","Service"],"name":"foo"},{"@type":["Product","Service"],"name":"bar"},{"@type":["Product","Service"],"name":"qux"}]}',
         ];
     }
 
@@ -138,17 +131,19 @@ final class MultipleTypeViewHelperTest extends ViewHelperTestCase
     public function itThrowsExceptionWhenUsedAsAChildTypeWithoutAsArgument(): void
     {
         $this->expectException(Exception::class);
-        $this->expectExceptionMessage('The child view helper of schema type "ProductStub / ServiceStub" must have an "-as" argument for embedding into the parent type');
+        $this->expectExceptionMessage('The child view helper of schema type "Product / Service" must have an "-as" argument for embedding into the parent type');
 
-        $template = <<< EOF
+        /** @var RenderingContextInterface $context */
+        $context = $this->get(RenderingContextFactory::class)->create();
+        $context->getTemplatePaths()->setTemplateSource(<<<TEMPLATE
             <schema:type.thing>
                 <schema:multipleType
-                    types="ProductStub,ServiceStub"
+                    types="Product,Service"
                     properties="{name: 'child name'}"
                 />
             </schema:type.thing>
-        EOF;
+        TEMPLATE);
 
-        $this->renderTemplate($template, []);
+        (new TemplateView($context))->render();
     }
 }
