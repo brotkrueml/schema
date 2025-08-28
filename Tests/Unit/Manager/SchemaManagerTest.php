9802f2d65f7f90c0b95fc8d842c90f000a7ce409
Chris Müller <coding@brotkrueml.dev>
2025-08-28 17:25:27 +0200

    chore: remove noop ReflectionProperty::setAccessible() call
    see: https://www.php.net/manual/en/reflectionproperty.setaccessible.php
    see: https://wiki.php.net/rfc/deprecations_php_8_5#deprecate_reflectionsetaccessible

--- Tests/Unit/Manager/SchemaManagerTest.php
+++ Tests/Unit/Manager/SchemaManagerTest.php
@@ -41,7 +41,6 @@ final class SchemaManagerTest extends TestCase
 
         $reflector = new \ReflectionClass(Renderer::class);
         $this->rendererTypes = $reflector->getProperty('types');
-        $this->rendererTypes->setAccessible(true);
 
         $applicationTypeStub = self::createStub(ApplicationType::class);
         $applicationTypeStub
